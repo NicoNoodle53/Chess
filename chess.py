f0d0ee9063ba3e771dfbc98c8be05e00f13cbb16
NicoNoodle53 <niconoodle65@gmail.com>
2025-06-04 12:52:56 -0600

    turns

--- chess.py
+++ chess.py
@@ -4,20 +4,15 @@ pygame.init()
 
 
 def main():
-
-
-
-
+    current_turn = "white"
     screen_width = 400
-    screen_height = 480
+    screen_height = 400
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("Chessboard")
     
     #colors
     white =(240, 217, 181)
     black = (181, 136, 99)
-    hightligh = (130,180,150)
-
     #load pieceimages
     piece_images = {
         'p': pygame.image.load('Chess/assets/black_pawn.png'),
@@ -67,9 +62,14 @@ def main():
 
                 piece =board[clicked_row][clicked_col] 
                 #check to see if we clicked on a piece
+                if piece !=' ':
+                    if piece.isupper():
+                        piece_color = "white"
+                    else:
+                        piece_color = "black"
                 if selected_piece_pos is None:
                     piece = board[clicked_row][clicked_col]
-                    if piece != ' ':
+                    if piece != ' ' and piece_color == current_turn:
                         selected_piece_pos = (clicked_row, clicked_col)
                         print(f"selected piece at: {selected_piece_pos}")
                 else:
@@ -83,6 +83,8 @@ def main():
                         board[end_row][end_col] = piece_to_move
                         selected_piece_pos = None
                         print(f"Moved {piece_to_move} from {(start_row, start_col)} to {(end_row, end_col)}")
+                        current_turn = 'black' if current_turn == 'white' else 'white'
+                        print(f"It's now {current_turn}'s turn.")
                     else:
                         #clicked on the same square, deselect
                         selected_piece_pos = None
@@ -102,13 +104,9 @@ def main():
             selected_square = None
         if selected_square:
             piece = board[clicked_row][clicked_col]
-            if piece != ' ':
-                selected_row, selected_col = selected_square
-                highlight_color = (255,255,0)
-                highlight_thickness = 3
-                highlight_rect = highlight_rect = get_highlight_rect(selected_row, selected_col, square_size)
-                pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
-
+            highlight_color = (255,255,0)
+            highlight_thickness = 5
+            
             if piece == 'p':
                 selected_row, selected_col = selected_square
 
@@ -132,13 +130,111 @@ def main():
 
                 highlight_rect = get_highlight_rect(selected_row - 1, selected_col, square_size)
                 pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                
+            if piece == 'b' or piece == "B":
+                for i in range(8):
+                    highlight_row_down,highlight_row_up, highlight_column_right, highlight_column_left = move(clicked_row,clicked_col, i)
+                    if not is_valid_square(highlight_row_down,highlight_column_right):
+                        break
+                    highlight_rect =get_highlight_rect(highlight_row_down,highlight_column_right, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                for i in range(8):
+                    highlight_row_down,highlight_row_up, highlight_column_right, highlight_column_left = move(clicked_row,clicked_col, i)
+                    highlight_rect =get_highlight_rect(highlight_row_down,highlight_column_left, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                for i in range(8):
+                    highlight_row_down,highlight_row_up, highlight_column_right, highlight_column_left = move(clicked_row,clicked_col, i)
+                    highlight_rect =get_highlight_rect(highlight_row_up,highlight_column_left, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                for i in range(8):
+                    highlight_row_down,highlight_row_up, highlight_column_right, highlight_column_left = move(clicked_row,clicked_col, i)
+                    highlight_rect =get_highlight_rect(highlight_row_up,highlight_column_right, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+            
+            if piece == "r" or piece == "R":
+                for i in range(8):
+                    highlight_row_down,highlight_row_up, highlight_column_right, highlight_column_left = move(clicked_row,clicked_col, i)
+                    highlight_rect = get_highlight_rect(highlight_row_up, clicked_col, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                    highlight_rect = get_highlight_rect(highlight_row_down, clicked_col, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                    highlight_rect = get_highlight_rect(clicked_row, highlight_column_left, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                    highlight_rect = get_highlight_rect(clicked_row, highlight_column_right, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
 
+            if piece == "q" or piece == "Q":
+                for i in range(8):
+                    highlight_row_down,highlight_row_up, highlight_column_right, highlight_column_left = move(clicked_row,clicked_col, i)
+                    highlight_rect = get_highlight_rect(highlight_row_up, clicked_col, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                    highlight_rect = get_highlight_rect(highlight_row_down, clicked_col, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                    highlight_rect = get_highlight_rect(clicked_row, highlight_column_left, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                    highlight_rect = get_highlight_rect(clicked_row, highlight_column_right, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                for i in range(8):
+                    highlight_row_down,highlight_row_up, highlight_column_right, highlight_column_left = move(clicked_row,clicked_col, i)
+                    highlight_rect =get_highlight_rect(highlight_row_down,highlight_column_right, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                for i in range(8):
+                    highlight_row_down,highlight_row_up, highlight_column_right, highlight_column_left = move(clicked_row,clicked_col, i)
+                    highlight_rect =get_highlight_rect(highlight_row_down,highlight_column_left, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                for i in range(8):
+                    highlight_row_down,highlight_row_up, highlight_column_right, highlight_column_left = move(clicked_row,clicked_col, i)
+                    highlight_rect =get_highlight_rect(highlight_row_up,highlight_column_left, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                for i in range(8):
+                    highlight_row_down,highlight_row_up, highlight_column_right, highlight_column_left = move(clicked_row,clicked_col, i)
+                    highlight_rect =get_highlight_rect(highlight_row_up,highlight_column_right, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                   
+            if piece == "k" or piece == "K":
+                    highlight_rect = get_highlight_rect(clicked_row + 1, clicked_col, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                    highlight_rect = get_highlight_rect(clicked_row - 1, clicked_col, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                    highlight_rect = get_highlight_rect(clicked_row, clicked_col - 1, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                    highlight_rect = get_highlight_rect(clicked_row, clicked_col + 1, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness) 
+                    highlight_rect = get_highlight_rect(clicked_row + 1, clicked_col + 1, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                    highlight_rect = get_highlight_rect(clicked_row + 1, clicked_col - 1, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                    highlight_rect = get_highlight_rect(clicked_row - 1, clicked_col - 1, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                    highlight_rect = get_highlight_rect(clicked_row - 1, clicked_col + 1, square_size)
+                    pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)               
+
+            if piece == "n" or piece == "N":
+                highlight_rect = get_highlight_rect(clicked_row + 2, clicked_col-1, square_size)
+                pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                highlight_rect = get_highlight_rect(clicked_row +2, clicked_col+1, square_size)
+                pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                highlight_rect = get_highlight_rect(clicked_row - 2, clicked_col - 1, square_size)
+                pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                highlight_rect = get_highlight_rect(clicked_row - 2, clicked_col + 1, square_size)
+                pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness) 
+                highlight_rect = get_highlight_rect(clicked_row + 1, clicked_col + 2, square_size)
+                pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                highlight_rect = get_highlight_rect(clicked_row + 1, clicked_col - 2, square_size)
+                pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                highlight_rect = get_highlight_rect(clicked_row - 1, clicked_col - 2, square_size)
+                pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)
+                highlight_rect = get_highlight_rect(clicked_row - 1, clicked_col + 2, square_size)
+                pygame.draw.rect(screen, highlight_color, highlight_rect, highlight_thickness)               
 
         pygame.display.flip()
 
     pygame.quit()
 
-def black_pawn_move(row,):
+def is_valid_square(row,col):
+    return 0 <= row < 8 and 0 <= col < 8
+
+def black_pawn_move(row):
     if row == 1:
         move = row + 2
     else:
@@ -151,6 +247,13 @@ def white_pawn_move(row):
     else:
         move = row - 1
     return move
+
+def move(row,column, i):
+    move_row_down = row + i
+    move_row_up = row - i
+    move_column_right = column + i
+    move_column_left = column - i
+    return move_row_down , move_row_up, move_column_right, move_column_left
 
 def get_highlight_rect(row, col, size):
     highlight_rect = pygame.Rect(col * size,row * size, size, size)
